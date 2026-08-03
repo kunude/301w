2cc015ca4c61dbe0a5cb4a8d36e6caa8d97a7506
kunude <85596880+kunude@users.noreply.github.com>
2026-08-03 17:43:22 +0800

    Disable git clone for luci-app-lucky
    Comment out the git clone command for luci-app-lucky.

---
+++ scripts/diy-part3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# DIY Script Part 1: 在更新 feeds 之前执行
+# 用于添加第三方软件源、修改默认配置等
+#
+
+echo 'src-git immortalwrt_pkg https://github.com/immortalwrt/packages.git' >> feeds.conf.default
+# echo 'src-git immortalwrt_luci https://github.com/immortalwrt/luci.git' >> feeds.conf.default
+
+# 示例：添加第三方软件源
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
+
+# 示例：修改默认 IP
+sed -i 's/192.168.1.1/192.168.35.1/g' package/base-files/files/bin/config_generate
+
+# 示例：修改默认主机名
+sed -i 's/ImmortalWrt/QNAP-301W/g' package/base-files/files/bin/config_generate
+
+# 301W 专用：确保 10G PHY 固件正确
+echo "确保 10G Aquantia PHY 支持..."
+
+# git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+
+exit 0
