168095f0f4ea28b678db601fb7829ecbb7a46951
kunude <85596880+kunude@users.noreply.github.com>
2026-08-27 19:44:51 +0800

    Comment out configuration commands in diy-part1.sh
    Comment out commands for modifying default IP, hostname, and cloning a repository.

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -11,14 +11,14 @@
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default
 
 # 示例：修改默认 IP
-sed -i 's/192.168.1.1/192.168.35.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.35.1/g' package/base-files/files/bin/config_generate
 
 # 示例：修改默认主机名
-sed -i 's/ImmortalWrt/QNAP-301W/g' package/base-files/files/bin/config_generate
+# sed -i 's/ImmortalWrt/QNAP-301W/g' package/base-files/files/bin/config_generate
 
 # 301W 专用：确保 10G PHY 固件正确
 echo "确保 10G Aquantia PHY 支持..."
 
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+# git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 exit 0
